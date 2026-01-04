b771714caea9d501e16e5f3315f731a0278d1bb2
BalkaSingh <khosabalkaransingh3@gmail.com>
2026-01-03 20:06:23 -0500

    Fixe Suit string

--- Suit.java
+++ Suit.java
@@ -3,22 +3,22 @@ public enum Suit{
 	//toString method for each : returns the string that represents the suit
 	HEART{
 		public String toString(){
-			return "♥️";
+			return "HEART";
 		}
 	},
 	SPADE{
 		public String toString(){
-			return "♠️";
+			return "SPADE";
 		}
 	},
 	CLUB{
 		public String toString(){
-			return "♣️";
+			return "CLUB";
 		}
 	},
 	DIAMOND{
 		public String toString(){
-			return "♦️";
+			return "DIAMOND";
 		}
 	}
 }
